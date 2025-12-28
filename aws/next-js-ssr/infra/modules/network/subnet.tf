90751eba076bb5fc97ab441d7cbac91185ab117f
HKRK8569 <hkrk3021@gmail.com>
2025-12-29 03:56:40 +0900

    サブネットの設定の追加

---
+++ aws/next-js-ssr/infra/modules/network/subnet.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "public" {
+  count                   = length(var.azs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-public-${var.azs[count.index]}"
+    Tier = "public"
+  })
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-private-${var.azs[count.index]}"
+    Tier = "private"
+  })
+}
